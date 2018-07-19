208d3966b376e2443b84ff53de9ca193484512b3
LilPacy <revivedtomorrow@gmail.com>
2018-07-19 23:29:04 +0900

    actions,reducer,createStoreの読み込み

--- fir-react-redux-todos/src/index.js
+++ fir-react-redux-todos/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import todo from './reducers';
+import { addTodo } from "./actions";
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+
+let store = createStore(todo);
+store.dispatch(addTodo('Hello World!'));
+console.log(store.getState());
 
 ReactDOM.render(
   <Provider>
